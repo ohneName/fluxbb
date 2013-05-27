7f23992ced89abbd5873f11c8e7ffc8588e80ee9
ozzyfant <ozzy2345de@gmail.com>
2013-05-27 22:17:58 +0200

    Übersetzung fixen

--- lang/German/jquery_captcha.php
+++ lang/German/jquery_captcha.php
@@ -2,7 +2,7 @@
 // Language definitions used by FluxBB Jquery Captcha 
 $lang_jquery_captcha = array(
 'Captcha legend'		=>	'Captcha',
-'Captcha description'	=>	'Bitte ziehe den Regler von rechts nach links.',
+'Captcha description'	=>	'Bitte ziehe den Regler von links nach rechts.',
 'Invalid captcha'		=>	'Inkorrektes Captcha.',
 'Javascript disabled'	=>	'Warnung: Um das Captcha zu benutzen, musst du JavaScript aktivieren.',
 );
